128dacbbeae597f1e60f33c641668ae24c7e0c9c
patelvishv179-ui <patelvishv179@gmail.com>
2026-05-25 17:04:31 +0530

    Day 6 Completed

---
+++ DAY 6/loopsinjs.js
@@ -0,0 +1,52 @@
+// Types of Loop : 
+// Entry Controll Loop , Exit Controll Loop
+
+//Entry Controll Loops :-> While And For Loop
+
+//Exit Controll Loops :-> Do While Loop
+
+//  ======= for loop ========
+
+//Syntax :
+// for(start; end->condition; change->increment or decrement;){}
+
+// Example :
+
+// let prompt = require("prompt-sync")();
+// let num = +prompt("Enter a Number : ");
+// for(let i=0; i<num; i++){
+//     console.log("Hello");
+// }
+
+// ==== while loop =======
+
+//Syntax
+// start;
+// while(condition/end){
+//     code
+    
+//     change
+// }
+
+//Example:
+// let i=0; //Start
+// while(i<10){ //Condition/end
+//     console.log("Hello " + i); //Code
+//     i++; //Change
+// }
+
+// ==== do while loop ===
+
+// Syntax
+
+// start;
+// do{
+//     code (Definetly Once Time Execute Even While Condition is False);
+//     change;
+// }while(condition / end);
+
+// let i=1;
+// do{
+//     console.log("Hello");
+//     i++
+// }while(i<1); //Condtion False Still Once Time Hello Print
